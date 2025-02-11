a9ddb62524adde0a8e3ac1f80d7edb3d02e00d12
Danny Semakov <semakovdaniel@gmail.com>
2025-02-11 01:21:57 -0500

    feat: add function to get names and rating of all players

--- players_database/players_model.py
+++ players_database/players_model.py
@@ -2,15 +2,18 @@
 from sqlalchemy import text
 from players_database.connection_manager import engine
 from players_database.connection_manager import SessionManager
-
+from players_database.db_tables import TitledChessPlayer
 
 
 #Maybe rename the class to PlayersManager/PlayerManager
 #Maybe have a separate module/class for querying the nicknames table.
+
+
+
 class PlayersModel(SessionManager):
   
-  def get_full_names(self):
-    sql_query = text("SELECT CONCAT(first_name, ' ', last_name) FROM chess_players")
+  def get_first_last_names(self):
+    sql_query = text("SELECT CONCAT(first_name, ' ', last_name) FROM titled_chess_players")
 
     print(type(sql_query))
 
@@ -19,3 +22,9 @@ class PlayersModel(SessionManager):
 
       #Convert CursorResult to
       return [i[0] for i in result.fetchall()]
+
+  def get_names_ratings(self):
+    """Returns a list of tuples containing name and rating info of all players. Each tuple
+    has the following format: (first_name, middle_name, last_name, rating)"""
+    return self.session.query(TitledChessPlayer.first_name, TitledChessPlayer.middle_name, TitledChessPlayer.last_name, TitledChessPlayer.rating).all()
+
