0dfcf4eb05ee53df46a03e846ee94549bd4e3144
Danny Semakov <semakovdaniel@gmail.com>
2025-03-09 14:29:29 -0400

    fix: correct requirements.txt path in install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ echo Installing virtual environment and packages
 sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
-cd .
+pip install -r RedditChessBot/requirements.txt
+
 
